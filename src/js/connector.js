3f334bbcee533a8083e7d525b0c2d98ed0660e21
Paul R. Carlson <carlsonpar@gmail.com>
2022-10-31 12:50:12 -0400

    fix for error

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,6 +1,6 @@
 console.log('hello world');
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
-trelloPowerUp.initialize({
+window.TrelloPowerUp.initialize({
 	'board-badges': function(t,options){
 		return[{
 			icon: BLACK_ROCKET_ICON,
