ce7fdc00c774d27279bc0b8f8e346b071aab9bc3
Paul R. Carlson <carlsonpar@gmail.com>
2022-10-31 13:00:17 -0400

    fix context

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,7 +3,7 @@ var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9
 window.TrelloPowerUp.initialize({
 	'board-buttons': function(t,options){
 		let context = t.getContext();
-		console.log(JSON.stringify(context))
+		console.log(context)
 		return[{
 			icon: BLACK_ROCKET_ICON,
 			text: 'Goodbeast GSheet integration'
