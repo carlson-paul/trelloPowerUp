5bab9789eb8912fc2a8bd095e4ac548ae5f28dd0
Paul R. Carlson <carlsonpar@gmail.com>
2022-11-28 15:55:33 -0500

    removal of requires and replaced with apicall

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,22 +1,16 @@
-const {google} = require('googleapis');
-const fetch = require('node-fetch');
-
 console.log('hello world');
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 window.TrelloPowerUp.initialize({
 	'board-buttons': function(t,options){
+		let apiCall = t.getRestApi();
 		let context = t.getContext();
-		let boardId = context['board'];
 		console.log(context['board'])
-		(async function(){
-			const cardResponse = await fetch(`https://api.trello.com/1/boards/${boardId}/cards?customFieldItems=true&key=8567e52ef0a5c3a9a4a76eb2722ea6d0&token=491a487812cc3fb1d1f188f9e693340640d287f36ef0bac4880c1fb707edbedc`);
-			let cardData = await cardResponse.text();
-			cardData = JSON.parse(cardData);
-			console.log(cardData);
-		}())
 		return[{
 			icon: BLACK_ROCKET_ICON,
 			text: 'Goodbeast GSheet integration'
 		}];
-	},
+	}, {
+		appKey: '8567e52ef0a5c3a9a4a76eb2722ea6d0',
+		appName: 'Goodbeast Googlesheet Powerup'
+	}
 })
