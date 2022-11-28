36cdc00ffc4857ed4125c27dd3d50fc186c4e09e
Paul R. Carlson <carlsonpar@gmail.com>
2022-11-28 16:20:34 -0500

    attempt new fetch

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,10 +4,17 @@ window.TrelloPowerUp.initialize({
 	'board-buttons': function(t,options){
 		let apiCall = t.getRestApi();
 		let context = t.getContext();
-		console.log(context['board'])
+		let boardId = context['board'];
+		return fetch(`'https://api.trello.com/1/boards/${boardId}/cards?customFieldItems=true&key=8567e52ef0a5c3a9a4a76eb2722ea6d0&token=491a487812cc3fb1d1f188f9e693340640d287f36ef0bac4880c1fb707edbedc'`)
+		.then(function(response) {
+			console.log(response.json());
+		}).then(
+			
+		)
+
 		return[{
 			icon: BLACK_ROCKET_ICON,
 			text: 'Goodbeast GSheet integration'
 		}];
-	}, 
+	}
 })
